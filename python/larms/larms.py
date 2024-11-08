d2f9659c3536ab9c91f2f51da3a77d031bfb00bd
Brian Dellinger <bdellinger@gmail.com>
2024-11-08 14:37:53 -0500

    More python and putting main() in parms->larms

--- python/larms/larms.py
+++ python/larms/larms.py
@@ -1,6 +1,8 @@
 #  What do I want this thing to do?
 #  Read in a file:  Restaurants / Score
 
+def main():
+    print("Testing")
 
 
 #  Create lottery where each restaurant has tickets = score
@@ -8,4 +10,6 @@
 #  Update file
 #       Restaurant that won to 1
 #       Others +2
+
+main ()
 #  Exit
