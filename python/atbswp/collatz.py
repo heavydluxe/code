2387350122d3e6f726614ad3c7c0cd98ef64d9c7
Brian Dellinger <bdellinger@gmail.com>
2024-11-14 18:44:21 -0500

    More python stuff

---
+++ python/atbswp/collatz.py
@@ -0,0 +1,26 @@
+import time, sys
+
+def main():
+    print("I'll do some collatz-y numerical things for you.")
+    
+    try:
+        var = int(input("Give me a number... It's best if your number is greater than 2: "))
+    except ValueError:
+        print("That's not a number, fool.")
+        sys.exit()       
+
+    while var != 1:
+        print(var)
+        var = collatz(var)
+
+    print(var)
+
+def collatz(number):
+    if number % 2 == 0:
+        number = int(number / 2)
+    else:
+        number = int(3 * number + 1)
+    return number
+    
+main()
+            
